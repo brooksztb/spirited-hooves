37bae339bc8f90d78454671bf8768089af489e38
Zachary.Brooks@flyfrontier.com <Zachary.Brooks@flyfrontier.com>
2019-01-17 16:14:43 -0700

    Moved navigation on mobile to right hand side menu button

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -30,8 +30,6 @@ export default {
             </v-list>
         </v-navigation-drawer>
         <v-toolbar app color="deep-purple darken-1" dark>
-            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
-            <v-spacer class="hidden-md-and-up"></v-spacer>
             <router-link to="/">
                 <v-toolbar-title to="/">{{appTitle}}</v-toolbar-title>
             </router-link>
@@ -39,6 +37,8 @@ export default {
             <v-btn flat class="hidden-sm-and-down" to="/services">Services</v-btn>
             <v-btn flat class="hidden-sm-and-down" to="/contact">Contact</v-btn>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
         </v-toolbar>
     </div>
     
